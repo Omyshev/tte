0694c1474c0436801202936283b93908869ba8f8
Omyshev <47952306+Omyshev@users.noreply.github.com>
2025-03-13 11:58:03 +0500

    Update youtube_button.js

--- youtube_button.js
+++ youtube_button.js
@@ -5,14 +5,19 @@
 
     function openYoutube() {
         // let url = "https://www.youtube.com";
-        // let proxiedUrl = `${PROXY_URL}/${encodeURIComponent(url)}`; // ✅ исправлено
+        // let proxiedUrl = `${PROXY_URL}/${encodeURIComponent(url)}`;
 
         // // Открываем YouTube через WebView Lampa или в браузере
         // Lampa.Platform.open(proxiedUrl);
     }
 
     function addYoutubeButton() {
-        let menu = Lampa.Start.menu(); // Получаем главное меню
+        let menu = $('.menu .menu__list'); // Находим контейнер меню
+
+        if (!menu.length) {
+            console.error("Меню Lampa не найдено!");
+            return;
+        }
 
         let youtubeButton = $('<li class="menu__item selector focus" data-action="youtube">')
             .append('<div class="menu__ico"><img src="https://www.youtube.com/s/desktop/9b235b1d/img/favicon.ico"></div>')
